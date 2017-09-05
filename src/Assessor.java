13b533c10d8c10bbc2500efa05dcab5bb73f2250
imwangi <31633915+imwangi@users.noreply.github.com>
2017-09-05 12:37:21 -0700

    Add files via upload
    Here's the structure of the accessor class complete. I'll do some polishing on the outputs but the functionality is all working.
    Test it and if anyone has any suggestions i'll be glad to improve on.

--- src/Assessor.java
+++ src/Assessor.java
@@ -1,75 +1,109 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 
 /** The Accessor Class Shows the questions and uses listeners to change the model
  *
  *
  */
-public class Assessor extends ItsPane implements ActionListener {
-
-    private JLabel questionLabel = new JLabel("Preferred Language");
-    private String[] comboWords = {"Java", "Python", "Swift", "Haskell"};
-    private JComboBox<String> comboBox = new JComboBox<>(comboWords);
-
-    private JCheckBox check1 = new JCheckBox("Object Oriented Programming", false);
-    private JCheckBox check2 = new JCheckBox("Functional Programming", false);
-    private JCheckBox check3 = new JCheckBox("Declarative Programming", false);
-
-    private JButton but1 = new JButton("Blue");
-    private JButton but2 = new JButton("Purple");
-    private JButton but3 = new JButton("Orange");
-    private JButton but4 = new JButton("Green");
-
-    private JTextArea textArea = new JTextArea();
-
+@SuppressWarnings("serial")
+public class Assessor  extends ItsPane implements ActionListener {
     public Assessor() {
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        setBackground(Color.WHITE);
-        but2.addActionListener(this);
-        but4.addActionListener(this);
-        updateComponent();
+        setBackground(Color.BLUE);
     }
+    
+    String names = "Hello World,\n My name is Ian Mwangi"; 
+    JTextField name= new JTextField(names);
+    
+    String options [] = {"Option 1","Option 2","Option 3","Option 4","Option 5"};
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	JComboBox combo= new JComboBox(options);
+   
+    
+    JTextArea txtArea = new JTextArea("Type Here ");
+    
+    JCheckBox check1= new JCheckBox("Option 1");
+    JCheckBox check2= new JCheckBox("Option 2");
+    JCheckBox check3= new JCheckBox("Option 3");
 
+    JRadioButton b1=new JRadioButton(" Option 1");    
+    JRadioButton b2=new JRadioButton(" Option 2"); 
+    JRadioButton b3=new JRadioButton(" Option 3"); 
+    
     @Override
-    void updateComponent() {
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
         removeAll();
-        switch (state) {
+		switch (state) {
             case 0:
             default:
-                add(new JLabel("Assessor.java"));
+                g.drawString("Accessor.java", 20, 20);    
+                name.setBounds(55,150,300,40);
+                add(name);
+ 
                 break;
             case 1:
-                questionLabel.setText("Preferred Language");
-                add(questionLabel);
-                add(comboBox);
+                g.drawString("Menu", 20, 20);
+               combo.setBounds(75,60,100,40); 
+                add(combo);
+                
+                combo.addActionListener(this);
+               
                 break;
             case 2:
-                questionLabel.setText("Programming Paradigms");
-                add(questionLabel);
-                add(check1);
-                add(check2);
+                g.drawString("CheckBoxes", 20, 20);
+                check1.setBounds(75,50,100,30); 
+                check2.setBounds(75,75,100,30); 
+                check3.setBounds(75,100,100,30); 
+                add(check1);  
+                add(check2); 
                 add(check3);
+                check1.addActionListener(this);
+                check2.addActionListener(this);
+                check3.addActionListener(this);
+                        
                 break;
             case 3:
-                questionLabel.setText("What color is the sky?");
-                add(questionLabel);
-                add(but1);
-                add(but2);
-                add(but3);
-                add(but4);
+            	
+                g.drawString("Buttons", 20, 20);
+                b1.setBounds(75,50,100,30); 
+                b2.setBounds(75,75,100,30); 
+                b3.setBounds(75,100,100,30); 
+                ButtonGroup item=new ButtonGroup();  
+                item.add(b1);
+                item.add(b2); 
+                item.add(b3);
+                add(b1);
+                add(b2);
+                add(b3);
+                b1.addActionListener(this); 
+                b2.addActionListener(this); 
+                b3.addActionListener(this);
+                             
                 break;
             case 4:
-                questionLabel.setText("What is your name?");
-                add(textArea);
+                g.drawString("Text field", 20, 20);
+                txtArea.setBounds(75,50,250,150); 
+                add(txtArea);
+                txtArea.setEditable(true);
+                
                 break;
         }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(this,"That's strange");
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(check3.isSelected())
+		System.out.println("Option 3 is correct");
+		else 
+			System.out.println("Option valid");
+		
+		
+			
+			
+	}
 }
