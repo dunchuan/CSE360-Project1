efb3e60e87d849281812ed6faf786a089a84ab0d
Robert Wasinger <robert@onepagego.com>
2017-09-05 13:42:26 -0700

    Merge of 13b533c

--- src/Assessor.java
+++ src/Assessor.java
@@ -9,87 +9,98 @@ import javax.swing.*;
  *
  *
  */
-@SuppressWarnings("serial")
 public class Assessor  extends ItsPane implements ActionListener {
-    public Assessor() {
-        setBackground(Color.BLUE);
-    }
-    
-    String names = "Hello World,\n My name is Ian Mwangi"; 
-    JTextField name= new JTextField(names);
-    
-    String options [] = {"Option 1","Option 2","Option 3","Option 4","Option 5"};
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	JComboBox combo= new JComboBox(options);
-   
-    
-    JTextArea txtArea = new JTextArea("Type Here ");
-    
-    JCheckBox check1= new JCheckBox("Option 1");
-    JCheckBox check2= new JCheckBox("Option 2");
-    JCheckBox check3= new JCheckBox("Option 3");
 
-    JRadioButton b1=new JRadioButton(" Option 1");    
-    JRadioButton b2=new JRadioButton(" Option 2"); 
-    JRadioButton b3=new JRadioButton(" Option 3"); 
-    
+    private String names = "Hello World,\n My name is Ian Mwangi";
+    private JTextField name= new JTextField(names);
+
+    private String options [] = {"Option 1","Option 2","Option 3","Option 4","Option 5"};
+    private JComboBox<String> combo= new JComboBox<>(options);
+
+
+    private JTextArea txtArea = new JTextArea("Type Here ");
+
+    private JCheckBox check1= new JCheckBox("Option 1");
+    private JCheckBox check2= new JCheckBox("Option 2");
+    private JCheckBox check3= new JCheckBox("Option 3");
+
+    private JRadioButton b1=new JRadioButton(" Option 1");
+    private JRadioButton b2=new JRadioButton(" Option 2");
+    private JRadioButton b3=new JRadioButton(" Option 3");
+
+    private JLabel questionLabel = new JLabel();
+
+    public Assessor() {
+        setBackground(Color.WHITE);
+        updateComponent();
+    }
+
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    void updateComponent() {
         removeAll();
-		switch (state) {
+        switch (state) {
             case 0:
             default:
-                g.drawString("Accessor.java", 20, 20);    
+                questionLabel.setText("Accessor.java");
+                add(questionLabel);
+
                 name.setBounds(55,150,300,40);
                 add(name);
- 
+
                 break;
             case 1:
-                g.drawString("Menu", 20, 20);
-               combo.setBounds(75,60,100,40); 
+                questionLabel.setText("Menu");
+                add(questionLabel);
+
+                combo.setBounds(75,60,100,40);
                 add(combo);
-                
+
                 combo.addActionListener(this);
-               
+
                 break;
             case 2:
-                g.drawString("CheckBoxes", 20, 20);
-                check1.setBounds(75,50,100,30); 
-                check2.setBounds(75,75,100,30); 
-                check3.setBounds(75,100,100,30); 
-                add(check1);  
-                add(check2); 
+                questionLabel.setText("CheckBoxes");
+                add(questionLabel);
+
+                check1.setBounds(75,50,100,30);
+                check2.setBounds(75,75,100,30);
+                check3.setBounds(75,100,100,30);
+                add(check1);
+                add(check2);
                 add(check3);
                 check1.addActionListener(this);
                 check2.addActionListener(this);
                 check3.addActionListener(this);
-                        
+
                 break;
             case 3:
-            	
-                g.drawString("Buttons", 20, 20);
-                b1.setBounds(75,50,100,30); 
-                b2.setBounds(75,75,100,30); 
-                b3.setBounds(75,100,100,30); 
-                ButtonGroup item=new ButtonGroup();  
+
+                questionLabel.setText("Buttons");
+                add(questionLabel);
+
+                b1.setBounds(75,50,100,30);
+                b2.setBounds(75,75,100,30);
+                b3.setBounds(75,100,100,30);
+                ButtonGroup item=new ButtonGroup();
                 item.add(b1);
-                item.add(b2); 
+                item.add(b2);
                 item.add(b3);
                 add(b1);
                 add(b2);
                 add(b3);
-                b1.addActionListener(this); 
-                b2.addActionListener(this); 
+                b1.addActionListener(this);
+                b2.addActionListener(this);
                 b3.addActionListener(this);
-                             
+
                 break;
             case 4:
-                g.drawString("Text field", 20, 20);
-                txtArea.setBounds(75,50,250,150); 
+                questionLabel.setText("Text field");
+                add(questionLabel);
+
+                txtArea.setBounds(75,50,250,150);
                 add(txtArea);
                 txtArea.setEditable(true);
-                
+
                 break;
         }
     }
@@ -98,12 +109,8 @@ public class Assessor  extends ItsPane implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		
 		if(check3.isSelected())
-		System.out.println("Option 3 is correct");
+		    System.out.println("Option 3 is correct");
 		else 
 			System.out.println("Option valid");
-		
-		
-			
-			
 	}
 }
