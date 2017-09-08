30305d5ccc8cd009a1f7ab9ebb9daf67fb97e554
Robert Wasinger <robert@onepagego.com>
2017-09-08 10:12:22 -0700

    Corrected resource retrieval for branch and moved Companion to src folder.

--- src/Companion.java
+++ src/Companion.java
@@ -10,9 +10,9 @@ import java.net.URL;
  *
  */
 
-public class Companion extends ItsPane {
-    private JLabel label = new JLabel("Companion.java");
-    private Image image;
+public class Companion extends ItsPane  {
+   private javax.swing.JLabel jLabel1 = new JLabel();
+   private Image image;
 
     public Companion() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
@@ -20,26 +20,30 @@ public class Companion extends ItsPane {
         updateComponent();
     }
 
-    @Override
     void updateComponent() {
         URL imagePath = null;
         removeAll();
+        
         switch(state) {
             default:
             case 0:
-                add(label);
+                add(jLabel1);
                 break;
             case 1:
-                imagePath = Companion.class.getClassLoader().getResource("happy.png");
+                initHappy();
+                //imagePath = Companion.class.getClassLoader().getResource("happy.png");
                 break;
             case 2:
-                imagePath = Companion.class.getClassLoader().getResource("thinking.png");
+                initThink();
+                //imagePath = Companion.class.getClassLoader().getResource("thinking.png");
                 break;
             case 3:
-                imagePath = Companion.class.getClassLoader().getResource("worry.png");
+                initWorry();
+                //imagePath = Companion.class.getClassLoader().getResource("worry.png");
                 break;
             case 4:
-                imagePath = Companion.class.getClassLoader().getResource("sad.png");
+                initSad();
+                //imagePath = Companion.class.getClassLoader().getResource("sad.png");
                 break;
         }
         if (imagePath != null) {
@@ -51,6 +55,121 @@ public class Companion extends ItsPane {
         }
     }
 
+    @SuppressWarnings("unchecked")                          
+    private void initHappy() {
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(Companion.class.getClassLoader().getResource("happy.png"))); // NOI18N
+        jLabel1.setMaximumSize(getPreferredSize());
+        jLabel1.setMinimumSize(getPreferredSize());
+        jLabel1.setPreferredSize(getPreferredSize());
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69))
+        );
+
+        jLabel1.getAccessibleContext().setAccessibleName("Happy");
+        jLabel1.getAccessibleContext().setAccessibleDescription("Happy Face");
+    }
+    
+     @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initThink() {
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(Companion.class.getClassLoader().getResource("thinking.png"))); // NOI18N
+        jLabel1.setMaximumSize(getPreferredSize());
+        jLabel1.setMinimumSize(getPreferredSize());
+        jLabel1.setPreferredSize(getPreferredSize());
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69))
+        );
+
+        jLabel1.getAccessibleContext().setAccessibleName("Thinking");
+        jLabel1.getAccessibleContext().setAccessibleDescription("Think Face");
+    }// 
+    
+    private void initWorry() {
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(Companion.class.getClassLoader().getResource("worry.png"))); // NOI18N
+        jLabel1.setMaximumSize(getPreferredSize());
+        jLabel1.setMinimumSize(getPreferredSize());
+        jLabel1.setPreferredSize(getPreferredSize());
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69))
+        );
+
+        jLabel1.getAccessibleContext().setAccessibleName("Worry");
+        jLabel1.getAccessibleContext().setAccessibleDescription("Worried Face");
+    }
+    
+    private void initSad() {
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(Companion.class.getClassLoader().getResource("sad.png"))); // NOI18N
+        jLabel1.setMaximumSize(getPreferredSize());
+        jLabel1.setMinimumSize(getPreferredSize());
+        jLabel1.setPreferredSize(getPreferredSize());
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69))
+        );
+
+        jLabel1.getAccessibleContext().setAccessibleName("Sad");
+        jLabel1.getAccessibleContext().setAccessibleDescription("Sad Face");
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
