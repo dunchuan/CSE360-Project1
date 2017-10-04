089bc00bfae9cf8a012b1fa87494627339626ef2
Jason <jas2070789@gmail.com>
2017-10-03 23:24:05 -0700

    Added a question.java that will have the questions for the the HW, and assessments   * Has main method for testing
    Finished Step 1,3, & 4 of numerical systems
      - Step 2 inwk

--- src/edu/asu/CSE360/_04/_03/Question.java
+++ src/edu/asu/CSE360/_04/_03/Question.java
@@ -1,17 +1,26 @@
 package edu.asu.CSE360._04._03;
-import java.util.Random;
+import java.util.Random;        // creates random number
+import java.util.*;             // for vector
 
 /**
  * Question Class will have the questions for the examples, tests, and quizzes of our ITS system
  *
  * Uses random number generation for binary problems.
  *
- * INWK=> Dec to Hex
+ *     Methods for Numerical Systems
+ * + binary()
+ * + binaryToHexGroups(String)  <- used to check step 1
+ *
+ * + binToDec(String)           <- used to check step 3
+ * + decToHex(int)              <- used to check step 4
+ *
+ * + getRandomNumberInRange(int, int)   <- used by binary to create a random binary number of random (2-8) length
+ * + leftPad(String, int, char)         <- add zeroes to the MSB of binary # to create groups of four digits
  * Has a main method for testing purposes.
  *
  *
  * Recitation Project 3
- * Completion time: 1 hours
+ * Completion time: 3 hours
  *
  * @author Jason Zellers * @version 1.0
  */
@@ -44,26 +53,68 @@ public class Question {
         return sb.toString();
     }
 
+    public static Vector binToHexGroups(String binary) {
+        String binaryFormat = binByFour(binary);
+        //System.out.println("Formatted: " +binaryFormat);
+
+        Vector hexGroups = new Vector();
+        int index = 0;
+        while (index < binaryFormat.length()) {
+            hexGroups.add(binaryFormat.substring(index, Math.min(index + 4,binaryFormat.length())));
+            index += 4;
+        }
+
+        return hexGroups;
+    }
+
     // This method is used to change the String binary (from binary()) to a decimal number (int)
-    public static int binToDecimal(String binary) {
+    public static int binToDec(String binary) {
         int dec = Integer.parseUnsignedInt(binary, 2);
 
         return dec;
     }
 
-    /*      TODO:
-    public static int binToHex(String binary) {
-
-        int groups = binary.length()/4;
-
+    // used to pad the hex groups with zeros to fill out the binary groups (groups of 4)
+    public static String leftPad(String originalString, int length, char padCharacter) {
+        String paddedString = originalString;
+        while (paddedString.length() < length) {
+            paddedString = padCharacter + paddedString;
+        }
+        return paddedString;
     }
-    */
+
+    public static String binByFour(String binary){
+        if (binary.length()%4 != 0)
+        {
+            int pad = 4- binary.length()%4;
+            int binLength = binary.length()+pad;
+            binary = leftPad(binary,binLength,  '0');           // to make binary string divisible by 4
+        }
+
+        return binary;
+    }
+
+
+    public static String decToHex(int decimal) {
+        String hexStr = Integer.toString(decimal,16);
+
+        return hexStr;
+    }
+
     public static void main (String[] args) {
 
         for (int i = 0; i < 10; i++) {
             String bin = binary();
-            System.out.println(i + " " +bin);
-            System.out.println("Decimal: " + binToDecimal(bin) + "\n");
+            System.out.println("Question: " + (i+1) + " \nBinary: " +bin);
+            System.out.println("Decimal: " + binToDec(bin));
+            System.out.println("Hex: " + decToHex(binToDec(bin)) + "\n");
+
+            System.out.println("Hex groups: ");
+            for(int j=0;j<binToHexGroups(bin).size();j++) {
+                System.out.print(binToHexGroups(bin).get(j).toString() + " \t ");
+            }
+            System.out.println("\n***************\n");
         }
+
     }
 }
